4b20e6a00489dda169984d65f741a94414752d66
Loreia <udlv20@gmail.com>
2012-11-27 12:56:05 +0100

    Fixed wrong language index bug

--- PowerEditor/src/ScitillaComponent/ScintillaEditView.cpp
+++ PowerEditor/src/ScitillaComponent/ScintillaEditView.cpp
@@ -759,14 +759,17 @@ void ScintillaEditView::setUserLexer(const TCHAR *userLangName)
 		}
 	}
 
-	// at the end (position SCE_USER_KWLIST_TOTAL) send id values
-	execute(SCI_SETPROPERTY, (WPARAM)"userDefine.udlName", reinterpret_cast<LPARAM>(userLangContainer->getName()));
-	execute(SCI_SETPROPERTY, (WPARAM)"userDefine.currentBufferID", reinterpret_cast<LPARAM>(_currentBufferID));
-
-	char intBuffer[10];
+ 	char intBuffer[15];
 	char nestingBuffer[] = "userDefine.nesting.00";
+
+	// at the end (position SCE_USER_KWLIST_TOTAL) send id values
+    itoa((int)userLangContainer->getName(), intBuffer, 10); // use numeric value of TCHAR pointer
+	execute(SCI_SETPROPERTY, (WPARAM)"userDefine.udlName", reinterpret_cast<LPARAM>(intBuffer));
+
+    itoa((int)_currentBufferID, intBuffer, 10); // use numeric value of BufferID pointer
+    execute(SCI_SETPROPERTY, (WPARAM)"userDefine.currentBufferID", reinterpret_cast<LPARAM>(intBuffer));
 	
-	//for (int i = 0 ; i < userLangContainer->_styleArray.getNbStyler() ; i++)
+	// for (int i = 0 ; i < userLangContainer->_styleArray.getNbStyler() ; i++)
 	for (int i = 0 ; i < SCE_USER_STYLE_TOTAL_STYLES ; i++)
 	{
 		Style & style = userLangContainer->_styleArray.getStyler(i);
