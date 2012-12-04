1e33841ee024928bf374c2441395c884cf9bbc87
Loreia <udlv20@gmail.com>
2012-12-04 14:25:18 +0100

    Bumped version number to 2.1

--- scintilla/include/SciLexer.h
+++ scintilla/include/SciLexer.h
@@ -201,7 +201,7 @@
 #define SCE_OBJC_QUALIFIER 21
 
 #define SCE_UDL_VERSION_MAJOR       2
-#define SCE_UDL_VERSION_MINOR       0
+#define SCE_UDL_VERSION_MINOR       1
 #define SCE_UDL_VERSION_BUILD       0
 #define SCE_UDL_VERSION_REVISION    0
 
